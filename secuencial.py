95e6a96c23ff554f91df4311491dd3868447de9b
Ana López <anamuyamiga@gmail.com>
2023-02-18 19:14:06 +0100

    primera funcion

--- secuencial.py
+++ secuencial.py
@@ -2,3 +2,11 @@ import random
 from time import sleep
 
 urls = ['a.com', 'b.com', 'c.com', 'd.com'] #Lista con las supuestas urls
+
+#Funcion que da un tiempo de duración aleatorio por url
+def links(url):
+    print('Empezando', url)
+    duracion = round(random.random(), 3)
+    sleep(duracion)
+    print('Termiunado', url, 'Tiempo', duracion, 's')
+    return url, duracion
